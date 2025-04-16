1085245981d735e5006e4db7448c5d2471ca515a
Nishad <mahmudnishad253@gmail.com>
2025-04-16 09:18:20 +0600

    ract form

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h3>Simple Form</h3>
+        <SimpleForm></SimpleForm>
+      </div>
+    </>
+  )
+}
+
+export default App
